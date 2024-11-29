b753b4a51f2bb524719e1e24f0758df1598934e8
gentarokai <kenny31999@gmail.com>
2024-11-29 14:58:53 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/grid_view_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.grid_view_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MaterialApp(
+        home: MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const PixabayPage();
+  }
+}
+
+class PixabayPage extends StatefulWidget {
+  const PixabayPage({super.key});
+
+  @override
+  State<PixabayPage> createState() => _PixabayPageState();
+}
+
+class _PixabayPageState extends State<PixabayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
